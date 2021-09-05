c18d0eb45f53c07128ee7322edef8c8eb0dfd3ae
Yuan Fu <casouri@gmail.com>
2021-09-05 16:54:00 -0700

    Support building C#

--- build.sh
+++ build.sh
@@ -18,18 +18,30 @@ cp "tree-sitter-${lang}/grammar.js" "tree-sitter-${lang}/src"
 cd "tree-sitter-${lang}/src"
 
 # The dynamic module's c source.
-sed "s/LANG/${lang}/g" tree-sitter-lang.in > "tree-sitter-${lang}.c"
+lang_c=$(echo "${lang}" | sed "s/-/_/g" )
+sed "s/LANG_C/${lang_c}/g;s/LANG/${lang}/g" tree-sitter-lang.in \
+    > "tree-sitter-${lang}.c"
 # Dump file of grammar definition to be included in the module.
 xxd -i < grammar.js > grammar.js.dump
 
 # Build.
-cc -c -I. *.c
-if test -f *.cc
+cc -c -I. parser.c
+# Compile scanner.c.
+if test -f scanner.c
 then
-    c++ -fPIC -I. -c *.cc
-    c++ -shared *.o -o "tree-sitter-${lang}.${soext}"
+    cc -fPIC -c -I. scanner.c
+fi
+# Compile scanner.cc.
+if test -f scanner.cc
+then
+    c++ -fPIC -I. -c scanner.cc
+fi
+# Link.
+if test -f scanner.cc
+then
+    c++ -fPIC -shared *.o -o "tree-sitter-${lang}.${soext}"
 else
-    cc -shared *.o -o "tree-sitter-${lang}.${soext}"
+    cc -fPIC -shared *.o -o "tree-sitter-${lang}.${soext}"
 fi
 
 # Copy out.
