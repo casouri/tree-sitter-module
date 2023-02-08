9ddb248b17a6b71cfc20be26a7992194d071b2dc
Yuan Fu <casouri@gmail.com>
2023-02-07 20:38:22 -0800

    Fix parallel build for macOS

--- batch.sh
+++ batch.sh
@@ -3,9 +3,48 @@
 set -u
 set -e
 
+languages=(
+    'bash'
+    'c'
+    'c-sharp'
+    'cmake'
+    'cpp'
+    'css'
+    'dockerfile'
+    'elixir'
+    'glsl'
+    'go'
+    'go-mod'
+    'heex'
+    'html'
+    'java'
+    'javascript'
+    'json'
+    'julia'
+    'make'
+    'markdown'
+    'org'
+    'perl'
+    'proto'
+    'python'
+    'ruby'
+    'rust'
+    'sql'
+    'toml'
+    'tsx'
+    'typescript'
+    'verilog'
+    'vhdl'
+    'wgsl'
+    'yaml'
+)
+
 if [ -z "${JOBS:-}" ]
 then
-    JOBS=$(nproc) || JOBS=1
+    for language in "${languages[@]}"
+    do
+        ./build.sh "${language}"
+    done
+else
+    printf "%s\n" "${languages[@]}" | xargs -P"${JOBS}" -n1 ./build.sh
 fi
-
-xargs -r -d '\n' -a "$PWD/languages.txt" -n1 -P"$JOBS" "$PWD/build.sh"
